5582fd0a086a22614c18141ac489762ba16b6508
Martin Kuehl <martin.kuehl@gmail.com>
2008-12-22 21:59:28 +0100

    • Use p instead of ul to display the repo list. • Let github-repos.js pass through JSLint without warnings. • Formatting.

--- js/github-repos.js
+++ js/github-repos.js
@@ -1,18 +1,22 @@
 $(function() {
-  var limit = 10        // how many repos to list
-  var login = 'mkhl'    // your username
+    var limit = 10;       // how many repos to list
+    var login = 'mkhl';   // your username
 
-  $.getJSON('http://github.com/api/v1/json/' + login + '?callback=?', function(data) {
-    var repos = $.grep(data.user.repositories, function() {
-      return !this.fork
-    })
+    $.getJSON('http://github.com/api/v1/json/' + login + '?callback=?', function(data) {
+        var repos = $.grep(data.user.repositories, function() {
+            return !this.fork;
+        });
 
-    repos.sort(function(a, b) {
-      return b.watchers - a.watchers
-    })
+        repos.sort(function(a, b) {
+            if (a.name < b.name)
+                return -1;
+            if (a.name > b.name)
+                return 1;
+            return 0;
+        });
 
-    $.each(repos.slice(0, limit), function() {
-      $('#repos').append('<li><a href="' + this.url + '">' + this.name + '</a></li>')
-    })
-  })        
-})
+        $.each(repos.slice(0, limit), function() {
+            $('#repos').append('<a href="' + this.url + '">' + this.name + '</a><br />');
+        });
+    });
+});
