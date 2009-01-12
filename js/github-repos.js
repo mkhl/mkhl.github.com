6d1d0aa73ca37e84fc4ccbd4d48a2b96aefe3ad2
Martin Kuehl <martin.kuehl@gmail.com>
2009-01-12 16:22:13 +0100

    • Formatting.

--- js/github-repos.js
+++ js/github-repos.js
@@ -1,24 +1,24 @@
 $(function() {
-    var limit = 10;       // how many repos to list
-    var login = 'mkhl';   // your username
+	var limit = 10;       // how many repos to list
+	var login = 'mkhl';   // your username
 
-    $.getJSON('http://github.com/api/v1/json/' + login + '?callback=?', function(data) {
-        var repos = $.grep(data.user.repositories, function() {
-            return !this.fork;
-        });
+	$.getJSON('http://github.com/api/v1/json/' + login + '?callback=?', function(data) {
+		var repos = $.grep(data.user.repositories, function() {
+			return !this.fork;
+		});
 
-        repos.sort(function(a, b) {
-            if (a.watchers != b.watchers)
-	            return b.watchers - a.watchers;
-            if (a.name < b.name)
-                return -1;
-            if (a.name > b.name)
-                return 1;
-            return 0;
-        });
+		repos.sort(function(a, b) {
+			if (a.watchers != b.watchers)
+				return b.watchers - a.watchers;
+			if (a.name < b.name)
+				return -1;
+			if (a.name > b.name)
+				return 1;
+			return 0;
+		});
 
-        $.each(repos.slice(0, limit), function() {
-            $('#repos').append('<a href="' + this.url + '">' + this.name + '</a><br />');
-        });
-    });
+		$.each(repos.slice(0, limit), function() {
+			$('#repos').append('<a href="' + this.url + '">' + this.name + '</a><br />');
+		});
+	});
 });
