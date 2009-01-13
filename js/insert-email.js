9566b9b9c1ea126e3275b585efbd3963b873a0d3
Martin Kuehl <martin.kuehl@gmail.com>
2009-01-13 23:19:47 +0100

    • Move rot13-“encrypted” data into textile source. • Use jQuery to find the data and “decrypt” it.

--- js/insert-email.js
+++ js/insert-email.js
@@ -1,23 +1,12 @@
-var mail = function () {
-	var self = new Object;
-	var emailLink = "<n uers=\"znvygb:znegva\056xhruy\100tznvy\056pbz\">znegva\056xhruy\100tznvy\056pbz<\057n>";
-	var emailString = "znegva\056xhruy\100tznvy\056pbz";
-
+$(function() {
 	function rot13Char (c) {
 		return String.fromCharCode((c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26);
 	}
-	function rot13String (str) {
+	function rot13 (str) {
 		return str.replace(/[a-zA-Z]/g, rot13Char);
 	}
 
-	self.insertLink = function (otherString) {
-		var otherString = otherString || "";
-		document.write(rot13String(emailLink) + otherString);
-	};
-	self.insertString = function (otherString) {
-		var otherString = otherString || "";
-		document.write(rot13String(emailString) + otherString);
-	};
-
-	return self;
-}();
+	$('.email').each(function(index) {
+		$(this).replaceWith(rot13($(this).html()));
+	});
+});
