3e511d82bbf55d56d7a70eb6641f7e49eac451e9
Martin Kuehl <martin.kuehl@gmail.com>
2009-01-14 01:24:33 +0100

    • Rewrite repo fetching code to facilitate jQuery and my util functions.

--- js/github-repos.js
+++ js/github-repos.js
@@ -3,22 +3,12 @@ $(function() {
 	var login = 'mkhl';   // your username
 
 	$.getJSON('http://github.com/api/v1/json/' + login + '?callback=?', function(data) {
-		var repos = $.grep(data.user.repositories, function() {
+		$(data.user.repositories).filter(function() {
 			return !this.fork;
-		});
-
-		repos.sort(function(a, b) {
-			if (a.watchers != b.watchers)
-				return b.watchers - a.watchers;
-			if (a.name < b.name)
-				return -1;
-			if (a.name > b.name)
-				return 1;
-			return 0;
-		});
-
-		$.each(repos.slice(0, limit), function() {
-			$('#repos').append('<a href="' + this.url + '">' + this.name + '</a><br />');
+		}).sort(function(a, b) {
+			return b.watchers - a.watchers;
+		}).slice(0, limit).each(function() {
+			$('#repos').append(this.name.truncate(25).link(this.url) + '<br />');
 		});
 	});
 });
