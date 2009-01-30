520776859244c6bee1ec1f8af1a3ba2f22b5e436
Martin Kuehl <martin.kuehl@gmail.com>
2009-01-30 15:24:49 +0100

    • Turn repo box into unsorted list. • Adapt github-repo script to add to that list.

--- js/github-repos.js
+++ js/github-repos.js
@@ -8,7 +8,7 @@ $(function() {
 		}).sort(function(a, b) {
 			return b.watchers - a.watchers;
 		}).slice(0, limit).each(function() {
-			$('#repos').append(this.name.truncate(25).link(this.url) + '<br />');
+			$('#repos ul').append(this.name.truncate(30).link(this.url).wrap('li'));
 		});
 	});
 });
