d016caf209b559fd712ec79ad3fd843b218b8f53
Martin Kuehl <martin.kuehl@gmail.com>
2009-02-14 00:37:20 +0100

    • Extract HTML builder function from github repo insertion code.

--- js/github-repos.js
+++ js/github-repos.js
@@ -2,13 +2,18 @@ $(function() {
     var limit = 10;       // how many repos to list
     var login = 'mkhl';   // your username
 
+    function buildHTML(repo) {
+        return repo.name.truncate(25).link(repo.url).wrap('li');
+    }
+    
+
     $.getJSON('http://github.com/api/v1/json/' + login + '?callback=?', function(data) {
         $(data.user.repositories).filter(function() {
             return !this.fork;
         }).sort(function(a, b) {
             return b.watchers - a.watchers;
         }).slice(0, limit).each(function() {
-            $('#repos ul').append(this.name.truncate(25).link(this.url).wrap('li'));
+            $('#repos ul').append(buildHTML(this));
         });
     });
 });
